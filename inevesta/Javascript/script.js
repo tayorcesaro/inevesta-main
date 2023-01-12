dc1aefa8075ce3f6b42314e6087eb77df877e95b
Kaypat4212 <109922476+Kaypat4212@users.noreply.github.com>
2023-01-12 19:04:04 +0100

    new changes and animatios added

--- inevesta/Javascript/script.js
+++ inevesta/Javascript/script.js
@@ -1,3 +1,25 @@
 let inevestahomepageword = document.getElementById("inevesta").innerHTML;
 
 document.getElementById("inevesta").innerHTML = inevestahomepageword.toUpperCase ();
+
+document.querySelector('.first-button').addEventListener('click', function () {
+
+    document.querySelector('.animated-icon1').classList.toggle('open');
+    });
+    document.querySelector('.second-button').addEventListener('click', function () {
+    
+    document.querySelector('.animated-icon2').classList.toggle('open');
+    });
+    document.querySelector('.third-button').addEventListener('click', function () {
+    
+    document.querySelector('.animated-icon3').classList.toggle('open');
+    });
+
+
+    
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
